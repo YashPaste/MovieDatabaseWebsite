9098fe650b734ff912caeb424525a9860a270642
YASH PASTE <yashshinde2110@gmail.com>
2021-07-24 04:42:53 +0530

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+    return (
+        <div>
+            <span onClick={()=> window.scroll(0,0)} className="header">🎬The Green Screen 🎥</span>
+        </div>
+    )
+}
+export default Header
